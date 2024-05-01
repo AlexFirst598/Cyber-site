b211ed3b34e1acdab642796dab649404fe8c1119
AlexFirst598 <141620329+AlexFirst598@users.noreply.github.com>
2024-05-01 20:26:15 +0300

    One step, finish

---
+++ assets/js/main.js
@@ -0,0 +1,16 @@
+barba.init({
+    transitions: [{
+      name: 'opacity-transition',
+      sync: true,
+      leave(data) {
+        return gsap.to(data.current.container, {
+          opacity: 0
+        });
+      },
+      enter(data) {
+        return gsap.from(data.next.container, {
+          opacity: 0
+        });
+      }
+    }]
+  });
